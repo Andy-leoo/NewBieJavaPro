0cd74b18e8b06c13fa6c8e69746a087726a3c4fd
Andy-leoo <865756596@qq.com>
2019-08-04 15:25:21 +0800

    初始化项目

---
+++ factory/src/main/java/com/newbie/NewBieAppliation.java
@@ -0,0 +1,17 @@
+package com.newbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @author Andy-J<br>
+ * @version 1.0<br>
+ * @createDate 2019/8/3 22:07 <br>
+ * @desc 主程序类
+ */
+@SpringBootApplication
+public class NewBieAppliation {
+    public static void main(String[] args) {
+        SpringApplication.run(NewBieAppliation.class, args);
+    }
+}
