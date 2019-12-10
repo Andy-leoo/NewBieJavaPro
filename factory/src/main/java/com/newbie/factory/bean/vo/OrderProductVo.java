bfa3dc2cd3497927744a95692a24fcf93ae347ee
Andy-leoo <865756596@qq.com>
2019-12-10 16:35:08 +0800

    门户-订单接口 1.创建订单2.获取订单的商品信息

---
+++ factory/src/main/java/com/newbie/factory/bean/vo/OrderProductVo.java
@@ -0,0 +1,40 @@
+package com.newbie.factory.bean.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author Andy-J<br>
+ * @version 1.0<br>
+ * @createDate 2019/12/10 15:42 <br>
+ * @desc 订单商品vo
+ */
+public class OrderProductVo {
+    private List<OrderItemVo> orderItemVoList;
+    private BigDecimal productTotalPrice;
+    private String imageHost;
+
+    public List<OrderItemVo> getOrderItemVoList() {
+        return orderItemVoList;
+    }
+
+    public void setOrderItemVoList(List<OrderItemVo> orderItemVoList) {
+        this.orderItemVoList = orderItemVoList;
+    }
+
+    public BigDecimal getProductTotalPrice() {
+        return productTotalPrice;
+    }
+
+    public void setProductTotalPrice(BigDecimal productTotalPrice) {
+        this.productTotalPrice = productTotalPrice;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
